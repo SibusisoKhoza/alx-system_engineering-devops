1e5d9e7bbd8be72dbef7a58b15abb20b1f4cd484
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-01-22 11:54:56 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Install a package
-package { 'puppet-lint':
+# 1-install_a_package.pp
+
+# Ensure the package resource type is available
+include ::apt
+
+# Define a package resource for Flask with the specified version
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip3',
+  require  => Class['apt'],
 }
