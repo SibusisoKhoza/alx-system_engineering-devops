0b68640fd034f659e965a2e32cd8b2a78a3a2bd4
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-01-23 15:50:33 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,4 @@
-# 1-install_a_package.pp
-
-# Ensure the package 'python3-pip' is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Install Flask version 2.1.0 using pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
