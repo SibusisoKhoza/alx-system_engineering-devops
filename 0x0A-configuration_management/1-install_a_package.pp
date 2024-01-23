e6fcfe6df790dcf3334fa001c68ac61e12ef1fdc
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-01-23 14:56:00 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,13 @@
 # 1-install_a_package.pp
 
-# Ensure the package resource type is available
-include ::apt
+# Ensure the package 'python3-pip' is installed
+package { 'python3-pip':
+  ensure => installed,
+}
 
-# Define a package resource for Flask with the specified version
+# Install Flask version 2.1.0 using pip3
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Class['apt'],
+  require  => Package['python3-pip'],
 }
