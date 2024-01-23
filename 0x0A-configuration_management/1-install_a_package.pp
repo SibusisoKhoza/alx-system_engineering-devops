cf3c1cec56fee3268ffacbbee89c70e9508fce5b
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-01-23 15:54:04 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,13 @@
-# 1-install_a_package.pp
+# install_flask.pp
 
-# Ensure the package 'python3-pip' is installed
+# Ensure pip3 is installed
 package { 'python3-pip':
   ensure => installed,
 }
 
 # Install Flask version 2.1.0 using pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin:/usr/bin:/bin',
+  require => Package['python3-pip'],
 }
