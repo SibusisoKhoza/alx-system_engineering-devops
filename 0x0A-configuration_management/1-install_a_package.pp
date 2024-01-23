21b1f49d5217ffc5c5540c0f714f810e1e57524f
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-01-23 15:51:56 +0200

    Revert "Update 1-install_a_package.pp"
    This reverts commit 0b68640fd034f659e965a2e32cd8b2a78a3a2bd4.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,13 @@
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# 1-install_a_package.pp
+
+# Ensure the package 'python3-pip' is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
