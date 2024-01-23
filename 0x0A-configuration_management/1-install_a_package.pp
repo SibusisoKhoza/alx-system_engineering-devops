0bb6260e1794e7ca7a66a0b740ac224931e1f537
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-01-23 15:56:54 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,18 @@
 # install_flask.pp
 
+# Ensure python3.8 is installed
+package { 'python3.8':
+  ensure => installed,
+}
+
 # Ensure pip3 is installed
 package { 'python3-pip':
   ensure => installed,
 }
 
-# Install Flask version 2.1.0 using pip3
+# Install Flask version 2.1.0 and Werkzeug version 2.1.1 using pip3
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
   path    => '/usr/local/bin:/usr/bin:/bin',
-  require => Package['python3-pip'],
+  require => [Package['python3.8'], Package['python3-pip']],
 }
