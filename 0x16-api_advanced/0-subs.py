b26c257db7c80054e56ed4a69208c892441bebd2
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-04-10 12:22:25 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
-"""A module that contains number_of_subscribers's function"""
-
+""" Returns the number of
+    subscribers for a given subreddit """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """A function that queries the Reddit API and returns the number of
-    subscribers for a given subreddit.
-
-    return: 0, If an invalid subreddit is given.
     """
+    A function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyRedditBot/1.0"}
+    res = requests.get(url, headers=headers)
+    subs = 0
 
-    user_agent = 'MyRedditApp/1.0 by u/Professional_Dog4632'
+    if res.status_code == 200:
+        data = res.json()
+        subs = data["data"]["subscribers"]
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': user_agent}
-
-    r = requests.get(url, headers=headers, allow_redirects=False)
-
-    if r.status_code != 200:
-        return 0
-
-    json = r.json()
-
-    # check if it is a subreddit.
-    kind = json.get("kind")
-    if kind != "t5":
-        return 0
-
-    # getting the data.
-    data = json.get("data")
-
-    number_of_subscribers = data.get("subscribers")
-    return (number_of_subscribers)
+    return subs
