604d00d9bbde6ad9d1c45d6ede4ada37c556442e
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-04-10 12:14:45 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
-"""How many subs"""
+"""A module that contains number_of_subscribers's function"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Number of subs"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'ALXReddit/0.1 by Sphax'}
-    try:
-        print(url)
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            subcount = data['data']['subscribers']
-            return subcount
-        else:
-            print(f"Error: {response.status_code}")
-            return 0
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+    """A function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit.
 
+    return: 0, If an invalid subreddit is given.
+    """
+
+    user_agent = 'MyRedditApp/1.0 by /u/Dredouane'
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': user_agent}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return 0
+
+    json = r.json()
+
+    # check if it is a subreddit.
+    kind = json.get("kind")
+    if kind != "t5":
+        return 0
+
+    # getting the data.
+    data = json.get("data")
+
+    number_of_subscribers = data.get("subscribers")
+    return (number_of_subscribers)
