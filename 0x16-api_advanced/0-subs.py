ec73851f1783d91a9e0a36563607399b61ca60d0
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-04-10 12:24:16 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,12 +10,10 @@ def number_of_subscribers(subreddit):
     subscribers for a given subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyRedditBot/1.0"}
-    res = requests.get(url, headers=headers)
-    subs = 0
-
-    if res.status_code == 200:
-        data = res.json()
-        subs = data["data"]["subscribers"]
-
-    return subs
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
