eb57df8369b046cd401b4137aec0bfa8bb254697
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-04-11 12:43:46 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,6 +12,7 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)
+    print(response)
     if response.status_code == 200:
         data = response.json()
         test = data['data']['subreddit_subscribers']
