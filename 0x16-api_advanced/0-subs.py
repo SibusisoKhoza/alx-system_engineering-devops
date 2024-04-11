261f5f251bd9075483631fe0d599085ff41c31b4
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-04-11 12:31:24 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,11 +9,11 @@ def number_of_subscribers(subreddit):
     A function that queries the Reddit API and returns the number of
     subscribers for a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = f"https://www.reddit.com/r/{subreddit}.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        return data['data']['subscribers']
+        return data['data']['subreddit_subscribers']
     else:
         return 0
